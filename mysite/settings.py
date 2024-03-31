993cb7180f278f7070ec70de62cb23284741e72d
EC2 Default User <ec2-user@ip-172-31-13-203.eu-west-1.compute.internal>
2024-03-31 11:38:28 +0000

    settings modified

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,13 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '24af20355ec14a48acaaa5a682646da4.vfs.cloud9.eu-west-1.amazonaws.com',
+    'x22180966wisespend-env.eba-dxnrbbmh.eu-west-1.elasticbeanstalk.com',
     ]
     
 #CSRF configuration
-CSRF_TRUSTED_ORIGINS = [
-    'https://24af20355ec14a48acaaa5a682646da4.vfs.cloud9.eu-west-1.amazonaws.com',
-    'x22180966wisespend-env.eba-dxnrbbmh.eu-west-1.elasticbeanstalk.com',
-    ]
+CSRF_TRUSTED_ORIGINS = ['https://24af20355ec14a48acaaa5a682646da4.vfs.cloud9.eu-west-1.amazonaws.com']
 
 # Application definition
 
