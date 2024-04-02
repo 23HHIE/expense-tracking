4886f76ac69b77c6e8e32ec267ebef0ed2b7404e
EC2 Default User <ec2-user@ip-172-31-13-203.eu-west-1.compute.internal>
2024-04-02 09:38:27 +0000

    comments

--- myexpense/models.py
+++ myexpense/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.utils.timezone import now
 
 
-# Create your models here.
+# the expense model in the database
 class Expense(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=100)
@@ -15,29 +15,29 @@ class Expense(models.Model):
     def __str__(self):
         return f"{self.type} - ${self.fee}"
 
-
+# the budget model in the database
 class Budget(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=20, decimal_places=2, default=0)
 
-    # Define a method to enable the user to add or modify their budgets
+    # a method to add or delete the budget
     def set_budget(self, amount):
         self.amount = amount
         self.save()
 
-    # Define a method to calculate the amount of the remaining budget
+    # a method to calculate the availble budget
     def get_remaining_budget(self):
-        # Fetch the current date
+        # store the current date
         today = now().date()
-        # Fetch the first date of the current month
+        # store the first date of the current month
         start_day = today.replace(day=1)
-        # Aggregate all the spending
+        # calculate the expense starts from the first day
         cost_this_month = Expense.objects.filter(
 
             user=self.user,
             payment_date__gte=start_day
         ).aggregate(total_expenses=models.Sum('fee'))['total_expenses'] or 0
-        # Calculate the current budget remaining by subtracting the cost from the budget
+        # calculate the remaining amount of the budget
         remaining_budget = self.amount - cost_this_month
         return round(remaining_budget, 2)
 
