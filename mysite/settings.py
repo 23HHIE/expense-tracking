e757b6ec5d904f9c257d5d3678709477e650d0a7
EC2 Default User <ec2-user@ip-172-31-13-203.eu-west-1.compute.internal>
2024-04-05 14:18:36 +0000

    revoke the secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,15 +11,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from mysite.secrets import SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3imnp62_i!@q!sb6byc6bggo+d*c4ck59$xydkl*0*de5skqkc'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
